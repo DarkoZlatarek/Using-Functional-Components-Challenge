3f5551dd66d3f827d6e4b78347d1684acc16edac
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-06-21 20:48:03 +0000

    Destructuring Props in PostItem.js

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -5,12 +5,13 @@ function PostItem(props) {
     const post = props
     return (
         post.savedPosts.map(post => {
+            const { name, title, description, image } = post;
             return (
-                <div key={post.title} className={css.SearchItem}>
-                    <p>{post.title}</p>
-                    <p>{post.name}</p>
-                    <img src={post.image} alt="Some weird stuff"></img>
-                    <p>{post.description}</p>
+                <div key={title} className={css.SearchItem}>
+                    <p>{title}</p>
+                    <p>{name}</p>
+                    <img src={image} alt="Some weird stuff"></img>
+                    <p>{description}</p>
                 </div>
             )
         })
