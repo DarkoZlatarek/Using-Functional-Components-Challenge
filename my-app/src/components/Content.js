148955c42c088e1b001cc7d891d45f3d02aced2a
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-06-24 19:49:07 +0000

    form in React - bug

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -7,32 +7,56 @@ import Loader from './Loader'
 export class Content extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       isLoaded: false
+      isLoaded: false,
+      posts: [],
     }
   }
 
   getData() {
     setTimeout(() => {
-        this.setState({
-          isLoaded: true,
-        })
+      this.setState({
+        isLoaded: true,
+      })
     }, 2000)
-}
-componentDidMount() {
+  }
+
+  componentDidMount() {
     this.getData()
-}
+    this.setState({
+      posts: savedPosts
+    })
+  }
+
+  handleChange = (event) => {
+    const name = event.target.value
+    const filteredPosts = posts.filter(post => {
+      return post.toLowerCase().includes(name)
+    })
+    this.setState({
+      posts: filteredPosts
+  })
+  }
 
   render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
-            <h1>My Photos</h1>
+          <h1>My Photos</h1>
+          <form>
+            <label htmlFor='searchInput'>Search:</label>
+            <input
+              id='searchInput'
+              type='search'
+              onChange={(event) => this.handleChange(event)}
+            />
+            <h4>posts found: {this.state.posts.length}</h4>
+          </form>
         </div>
 
         <div className={css.SearchResult}>
-            {this.state.isLoaded ? (<PostItem savedPosts={savedPosts}/>) : (<Loader />)}
+          {this.state.isLoaded ? (<PostItem savedPosts={this.state.posts} />) : (<Loader />)}
         </div>
       </div>
     )
